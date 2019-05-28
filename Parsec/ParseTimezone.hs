9a5b0b802e3eb4b488401479b228309cf0f44267
David Frohlingsdorf <2079884f@student.gla.ac.uk>
2019-05-28 06:30:28 +0200

    Fixed Parsec library

--- Parsec/ParseTimezone.hs
+++ Parsec/ParseTimezone.hs
@@ -11,7 +11,7 @@ parseTZ = (char 'Z' >> return 0) <|> pOffset <|> return 0
   where
     pMinutes = (char ':' >> parseDigits 2) <|> parseDigits 2
     pOffset = do
-      d <- (char '+' >> return 1) <|> (char '-' >> return (-1))
+      d <- (char '+' >> return (-1)) <|> (char '-' >> return 1)
       h <- parseDigits 2
       m <- pMinutes <|> return 0
       return (d * (h*60*60 + m*60))
--- Parsec/main.hs
+++ Parsec/main.hs
@@ -3,6 +3,9 @@ import ParseIso8601
 -- Test with
 -- date -d @$(./main < input.txt)
 
+-- Reverse
+-- date -d "2019-03-10T12:36:49-02:00" +%s
+
 play :: String -> IO ()
 play inp = case parseIso8601 inp of
              { Left err -> print err
