7a9363554ecc0d63128d62758d1946ed417d137e
David Frohlingsdorf <2079884f@student.gla.ac.uk>
2019-08-02 20:14:11 +0200

    Added some unix conversion functionality to Util

--- Util.hs
+++ Util.hs
@@ -7,8 +7,32 @@ module JTPrettyTime.Util
 
 import Data.Time.Calendar
 import Data.Time.Format
+import Data.Time.Clock
+import Data.Time.Clock.POSIX
+import Data.Time.LocalTime
 
 type Iso8601 = String
+
+-- Converts for UTC timezone
+-- From https://stackoverflow.com/questions/44905138
+unixToUTCDay :: Integer -> Day
+unixToUTCDay = utctDay . posixSecondsToUTCTime . fromIntegral
+
+unixToSpecificDay :: Integer -> TimeZone -> Day
+unixToSpecificDay t tz = localDay l -- TODO some nicer composition like unixToUTCDay possible
+  where
+    utc = posixSecondsToUTCTime $ fromIntegral t
+    l = utcToLocalTime tz utc
+
+unixToLocalDay :: Integer -> IO Day
+unixToLocalDay t = unixToSpecificDay t <$> getCurrentTimeZone
+
+-- The timezone is the offset from UTC in minutes
+-- For example: CET=60, PST=-480
+unixToDay :: Integer -> Integer -> Day
+unixToDay t tz = unixToUTCDay t'
+  where
+    t' = t + tz * 60
 
 parseIso8601 :: ParseTime t => Iso8601 -> t
 parseIso8601 = parseTimeOrError True defaultTimeLocale spec
